d5eeb29c327cc0c5fb9726216ada5be88f1d4b34
Salma Khaled <salmakhaled.skk@gmail.com>
2026-03-30 23:51:22 +0200

    fix: remove unused toggleLang and dispatch

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,39 +1,20 @@
 import React from "react";
 import "../Home/Home.css";
 import { useNavigate } from "react-router-dom";
-import {  useSelector } from "react-redux";
-// import { changeLanguage } from "../../Store/Slices/Language";
+import { useSelector } from "react-redux";
 
 function Home() {
-  //hna mskt el state el kbeera w e5trt na 3awza a3rd eh f e5trt el language
- const Lang= useSelector((state)=>
-    state.sliceLanguage.language)
-    console.log(Lang)
-  
+  const Lang = useSelector((state) => state.sliceLanguage.language);
+  console.log(Lang);
+
   const navigate = useNavigate();
-  //dispatch 3mltha 3lshan a3ml change ll state language
-  // const dispatch= useDispatch()
-  // const toggleLang = () => {
-  //   if (Lang === "ar") {
-      
-  //     dispatch(changeLanguage("en")) 
-  //   } else {
-  //     dispatch(changeLanguage("ar"))
-  // }}
+
   return (
     <>
-    {/* <h3>language is :{Lang}</h3> */}
-    <button className="btn btn-success" onClick={() => navigate("/products")}>
+      <button className="btn btn-success" onClick={() => navigate("/products")}>
         Explore Movies
       </button>
-    <div className="home">
-
-    </div>
-        {/* <button className="btn btn-success" onClick={()=>{toggleLang()}} >
-       changeLanguage
-      </button> */}
-      {/* <p>home</p> */}
-
+      <div className="home"></div>
     </>
   );
 }
